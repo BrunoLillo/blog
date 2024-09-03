7d1f025287da93f3579266954a325cc6d773ebd2
BrunoLillo <bruno_lillo@hotmail.com>
2024-09-03 11:11:39 +0200

    configurações do post

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,5 +1,9 @@
+#type: ignore
 from django.contrib import admin
 from blog.models import Tag,Category, Page, Post
+from django_summernote.admin import SummernoteModelAdmin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -20,7 +24,8 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields ={ "slug" : ('name',),}
 
 @admin.register(Page)
-class PageAdmin(admin.ModelAdmin):
+class PageAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     list_display ='id','title', 'is_published'
     list_display_links ='title',
     search_fields ='id','slug','title', 'content',
@@ -31,16 +36,34 @@ class PageAdmin(admin.ModelAdmin):
     prepopulated_fields ={ "slug" : ('title',),}
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display ='id', 'title', 'is_published', 'created_by',
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+    list_display = 'id', 'title', 'is_published',  'created_by',
     list_display_links = 'title',
-    search_fields = 'id', 'slug', 'title', 'execerpt', 'content','cover',
+    search_fields = 'id', 'slug', 'title', 'excerpt', 'content',
     list_per_page = 50
-    list_filter='category', 'is_published',
-    list_editable= 'is_published',
+    list_filter = 'category', 'is_published',
+    list_editable = 'is_published',
     ordering = '-id',
-    readonly_fields= 'created_at', 'updated_at', 'updated_by', 'created_by',
+    readonly_fields = ('created_at', 'created_by', 'updated_at', 
+        'updated_by','link')
     prepopulated_fields = {
         "slug": ('title',),
     }
-    autocomplete_fields= 'tags','category',
+    autocomplete_fields = 'tags', 'category',
+
+    def link(self,obj):
+        if not obj.pk:
+            return '-'
+        
+        url_do_post = obj.get_absolute_url()
+        safe_link =mark_safe(f'<a target="_blank" href="{url_do_post}">Ver Post</a>')
+        return  safe_link
+    
+    def save_model(self, request, obj, form, change):
+        if change:
+           obj.update_by = request.user 
+        else: 
+            obj.created_by = request.user
+
+        obj.save()
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from blog.models import Post
 
-posts = list(range(100))
+PER_PAGE=9
 
 def index(request):
-    paginator = Paginator(posts,9)
+    posts = Post.objects.get_published()
+
+    paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
@@ -16,29 +19,58 @@ def index(request):
         }
     )
 
-def page(request):
-    paginator = Paginator(posts,9)
+def created_by(request, author_pk):
+    posts = Post.objects.get_published().filter(created_by__pk=author_pk)
+
+    paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj':page_obj,
+        }
+    )
+
+def category(request, slug):
+    posts = Post.objects.get_published().filter(category__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj':page_obj,
+        }
+    )
+
+def page(request,slug):
+    
     return render(
         request,
         'blog/pages/page.html',
         {
-            'page_obj':page_obj,
+          #  'page_obj':page_obj,
         }
 
     )
-def post(request):
-    paginator = Paginator(posts,9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
+def post(request,slug):
+    post=(
+        Post.objects.get_published()
+        .filter(slug=slug)
+        .first()
+    )
+    
     return render(
         request,
         'blog/pages/post.html',
         {
-            'page_obj':page_obj,
+            'post' : post,
         }
 
     )
