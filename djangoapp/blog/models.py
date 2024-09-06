a5a36b5891f593fd2b992feba251b94647afcc35
BrunoLillo <bruno_lillo@hotmail.com>
2024-09-06 11:12:27 +0200

    Based View category,post,createdBy

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -63,15 +63,20 @@ class Page(models.Model):
     title = models.CharField(max_length=65,)
     slug = models.SlugField(
         unique=True,
-        default=None, null=True, blank=True, max_length=255,
+        default="", null=True, blank=True, max_length=255,
     )
     is_published = models.BooleanField(default=False,
         help_text = 'Essse campo deve estar marcado para a pagina ser exibida publicamente')
     content = models.TextField()
+
+    def get_absolute_url(self):
+        if not self.is_published:
+            return reverse('blog:index')
+        return reverse('blog:page', args=(self.slug,))
     
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugfy_new(self.title,3)
+            self.slug = slugfy_new(self.title,4)
         return super().save(*args, **kwargs)
     
     def __str__(self) -> str:
--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from blog.views import index,page,post,created_by,category
+from blog.views import (PostListView,page,post,CreatedByListView,
+    CategoryListView,tag,search)
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', PostListView.as_view(), name='index'),
     path('post/<slug:slug>/', post, name='post'),
     path('page/<slug:slug>/', page, name='page'),
-    path('created_by/<int:author_pk>/', created_by, name='created_by'),
-    path('category/<slug:slug>/', category, name='category'),
+    path('created_by/<int:author_pk>/', CreatedByListView.as_view(), name='created_by'),
+    path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
+    path('tag/<slug:slug>/', tag, name='tag'),
+    path('search/', search, name='search'),
 ]
 
