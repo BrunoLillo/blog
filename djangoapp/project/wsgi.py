d594e4019d5f5f1917b91f543e602853055d2b8e
BrunoLillo <bruno_lillo@hotmail.com>
2024-09-06 15:54:19 +0200

    detalhes final com axes

--- djangoapp/project/wsgi.py
+++ djangoapp/project/wsgi.py
@@ -6,10 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-
 import os
-
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv #type: ignore
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR.parent/ 'dotenv_files' / '.env', override=True)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
